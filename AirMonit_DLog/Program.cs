688b4fc4a904b56f81d3e8cdfd0c81d976188cbe
Goncaloo42 <goncalito17@hotmail.com>
2017-11-27 10:03:49 +0000

    New DU, DLog

---
+++ AirMonit_DLog/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace AirMonit_DLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MqttClient mClient = new MqttClient("127.0.0.1");
+            string[] topics = { "no2", "co", "o3" };
+            mClient.Connect(Guid.NewGuid().ToString());
+            if (!mClient.IsConnected)
+            {
+                Console.WriteLine("Error connecting to message broker...");
+                return;
+            }
+
+            mClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+            //Subscribe to topics
+
+            byte[] qosLevels = { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE ,
+            MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE};
+            mClient.Subscribe(topics, qosLevels);
+            Console.ReadKey();
+            if (mClient.IsConnected)
+            {
+                mClient.Unsubscribe(topics); //Put this in a button to see notif!
+                mClient.Disconnect(); //Free process and process's resources
+            }
+        }
+
+        private static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            string data = Encoding.UTF8.GetString(e.Message);
+            Console.WriteLine(data);
+        }
+    }
+}

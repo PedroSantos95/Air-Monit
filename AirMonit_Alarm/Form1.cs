4a54d460b37a2fbdd3aba272bce9d59ce1d8a337
PedroSantos95 <pedromverasantos@gmail.com>
2017-12-04 16:58:10 +0000

    Alarm Subscribed

--- AirMonit_Alarm/Form1.cs
+++ AirMonit_Alarm/Form1.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Serialization;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
 
 namespace AirMonit_Alarm
 {
     public partial class Form1 : Form
     {
+        private static Sensor sensor;
+        private static MqttClient m_cClient = new MqttClient("127.0.0.1");
+        private static List<String> topics = new List<String>();
+
         public Form1()
         {
             InitializeComponent();
@@ -20,7 +29,17 @@ namespace AirMonit_Alarm
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            //Se ficheiro xml for válido então pode começar a receber dados
+            XMLHandler xmlHandler = new XMLHandler("trigger-rules.xml", "trigger-rules.xsd");
+            if (xmlHandler.ValidadeXml())
+            {
+                Thread thread = new Thread(receiveSensorData);
+                thread.Start();
+            }
+            else
+            {
+                MessageBox.Show("XML Invalid file!");
+            }
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
@@ -30,6 +49,7 @@ namespace AirMonit_Alarm
             if (checkBox1.Checked && checkerror == 0)
             {
                 XMLWriter();
+                subscribeTopics();
                 checkBox1.Text = "Off";
             }
             else{
@@ -286,9 +306,109 @@ namespace AirMonit_Alarm
             doc.Save("trigger-rules.xml");
         }
 
-        private void triggerAlarm()
+        private static void XMLReader(Sensor sensor)
         {
+             string alarmMessage = "";
 
+            XmlTextReader xmlReader = new XmlTextReader("trigger-rules.xml");
+            while (xmlReader.Read())
+            {
+                alarmMessage += sensor.Value;
+                
+            }
+            xmlReader.Close();
+
+            m_cClient.Publish(alarmMessage, Encoding.UTF8.GetBytes(sensor.Serialize()));
+        }
+
+        private void subscribeTopics()
+        {
+            if (checkBox2.Checked)
+            {
+                topics.Add("no2");
+                m_cClient.Subscribe(new String[] { "no2" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+            else
+            {
+                if (topics.Contains("no2"))
+                {
+                    topics.Remove("no2");
+                    m_cClient.Unsubscribe(new String[] { "no2" });
+                }
+            }
+
+            if (checkBox3.Checked)
+            {
+                topics.Add("co");
+                m_cClient.Subscribe(new String[] { "co" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+            else
+            {
+                if (topics.Contains("co"))
+                {
+                    topics.Remove("co");
+                    m_cClient.Unsubscribe(new String[] { "co" });
+                }
+            }
+
+            if (checkBox4.Checked)
+            {
+                topics.Add("o3");
+                m_cClient.Subscribe(new String[] { "o3" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            }
+            else
+            {
+                if (topics.Contains("o3"))
+                {
+                    topics.Remove("o3");
+                    m_cClient.Unsubscribe(new String[] { "o3" });
+                }
+            }
+        }
+
+        private void receiveSensorData()
+        {
+            try
+            {
+                m_cClient = new MqttClient("127.0.0.1");
+                m_cClient.Connect(Guid.NewGuid().ToString());
+                if (!m_cClient.IsConnected)
+                {
+                    Console.WriteLine("Error connecting to message broker...");
+                    return;
+                }
+
+                //Specify events we are interest on
+                //New Msg Arrived
+                m_cClient.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+
+        static void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            try
+            {
+                String xmlString = Encoding.UTF8.GetString(e.Message);
+
+                StringReader stringReader = new StringReader(xmlString);
+                XmlSerializer serializer = new XmlSerializer(typeof(Sensor), new XmlRootAttribute("sensor"));
+
+                sensor = (Sensor)serializer.Deserialize(stringReader);
+
+                XMLReader(sensor);
+                Console.WriteLine("Valor: " + sensor.Value + " Nome: " + sensor.SensorName + " Cidade: " + sensor.SensorCity);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
---
+++ AirMonit_Alarm/Sensor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace AirMonit_Alarm
+{
+    [XmlRoot("alarm")]
+    public class Sensor
+    {
+        [XmlElement("sensor_id")]
+        public int SensorID { get; set; }
+        [XmlElement("sensor_name")]
+        public string SensorName { get; set; }
+        [XmlElement("sensor_value")]
+        public double Value { get; set; }
+        [XmlElement("sensor_date")]
+        public string DateTime { get; set; }
+        [XmlElement("sensor_city")]
+        public string SensorCity { get; set; }
+
+        public Sensor()
+        {
+        }
+
+        internal string Serialize()
+        {
+            if (this == null)
+            {
+                return string.Empty;
+            }
+            try
+            {
+                var xmlserializer = new XmlSerializer(typeof(Sensor));
+                var stringWriter = new StringWriter();
+                using (var writer = XmlWriter.Create(stringWriter))
+                {
+                    xmlserializer.Serialize(writer, this);
+                    return stringWriter.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred", ex);
+            }
+        }
+
+    }
+}
